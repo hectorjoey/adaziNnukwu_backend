07dbe04cc814d7a757b7f669c01c1179912b59c4
Hector Stanley <stanleychike@yahoo.com>
2020-01-29 18:07:29 +0100

    commit

---
+++ src/main/java/com/example/hector/adazi/AdaziApplication.java
@@ -0,0 +1,13 @@
+package com.example.hector.adazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hector/adazi/AdaziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hector.adazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
